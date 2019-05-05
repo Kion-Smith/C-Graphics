938826f76ffb0a03c9f5665ed4f46712a5cf6f7f
Kion Smith <kion_smith@hotmail.com>
2019-05-05 13:03:12 -0500

    adding glew + escape button to close window

--- GLFW/Learning Opengl/Learning Opengl/main.cpp
+++ GLFW/Learning Opengl/Learning Opengl/main.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
+
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
 
 int main(void)
 {
+	//glfwWindowHint(GLFW_SAMPLES, 1);
 	GLFWwindow* window;
 
 	/* Initialize the library */
 	if (!glfwInit())
+	{
 		return -1;
+	}
 
 	/* Create a windowed mode window and its OpenGL context */
 	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
@@ -20,10 +26,38 @@ int main(void)
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
 
-	/* Loop until the user closes the window */
+	//init glew; we use glow to use new opengl functions
+	glewExperimental = true;
+	if (glewInit() != GLEW_OK)
+	{
+		return -1;
+	}
+
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE); // get keys for window
+
+	do {
+		
+		glClear(GL_COLOR_BUFFER_BIT); //clear screen
+
+		//drwa trigangle
+		glBegin(GL_TRIANGLES);
+		glVertex2f(-.5f, -.5f);
+		glVertex2f(.0f, .5f);
+		glVertex2f(.5f, -.5f);
+		glEnd();
+		
+		//swap buffer then poll
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+	} 
+	while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0); //close on escape
+
+	/* OLD LOOP no input
+	/* Loop until the user closes the window 
 	while (!glfwWindowShouldClose(window))
 	{
-		/* Render here */
+		/* Render here 
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		
@@ -35,13 +69,14 @@ int main(void)
 		glEnd();
 		
 
-		/* Swap front and back buffers */
+		/* Swap front and back buffers 
 		glfwSwapBuffers(window);
 
-		/* Poll for and process events */
+		/* Poll for and process events
 		glfwPollEvents();
 	}
 
-	glfwTerminate();
+	
+	glfwTerminate();*/	
 	return 0;
 }
